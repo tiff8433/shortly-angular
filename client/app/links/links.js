c2e18d274aff71b2101ec87fd5a9b5a8f56ea978
tiff8433 <tiffany.l.huang@gmail.com>
2015-12-31 07:02:21 -0800

    Implementing linkcontroller

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,11 +2,14 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
-  console.log($scope);
-  console.log(Links);
   $scope.data = {};
   $scope.data.links;
   $scope.getLinks = function(){
     Links.getLinks()
-  }
+    .then(function(links){
+      //getting link, do what with it? 
+      console.log('scope data links', $scope.data.links);
+    });
+  };
+  $scope.getLinks(); 
 });
